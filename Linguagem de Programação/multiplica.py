6b436453b2b46def86e01c5f31f19462bcc22d64
Euclides Paim <euclidespaim@gmail.com>
2026-03-12 13:25:02 -0300

    Repositorio inicializado

--- Linguagem de Programação/multiplica.py
+++ Linguagem de Programação/multiplica.py
@@ -2,4 +2,10 @@
 #para o usuário, multiplique eles e escreva
 #o resultado na tela.
 
+x = float(input("Digite o primeiro número: "))
+y = float(input("Digite o segundo número: "))
 
+
+resultado = x * y
+
+print("O resultado da multiplicação é: ", resultado)
